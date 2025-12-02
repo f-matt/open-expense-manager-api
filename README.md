afee22f63bb2300b7ba73f6660fc5418f29ee11a
Fernando Mattioli <fernando.mattioli@ebserh.gov.br>
2025-12-01 21:45:21 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Configuration
+
+## Virtual environment
+
+```
+$ python3 -m venv venv
+$ source venv/bin/activate
+$ pip install -r requirements.txt
+```
+
+## Running
+
+```
+$ fastapi dev main.py
+```
---
+++ main.py
@@ -0,0 +1,17 @@
+#!/bin/python
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+
+class Credentials(BaseModel):
+    username: str
+    password: str
+
+app = FastAPI()
+
+@app.post("/login")
+async def login(credentials: Credentials):
+    if credentials.username == "admin" and credentials.password == "123":
+        return {"message" : "ok"}
+
+    raise HTTPException(status_code=401, detail="Invalid username/password.")
